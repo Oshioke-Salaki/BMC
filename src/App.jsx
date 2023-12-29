7619dde5a6e3332f76dbbdd1d75ccc4d21ade8bb
Oshioke-Salaki <salaki1902@gmail.com>
2023-12-29 03:28:22 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import StakeContainer from "./ui/complex/StakeContainer";
 import Navbar from "./ui/components/Navbar";
-import SelectTokenModal from "./ui/components/SelectTokenModal";
 
 function App() {
   return (
-    <div className="relative h-full w-screen">
-      <SelectTokenModal />
+    <div className="h-full w-screen">
       <Navbar />
       <div className="flex w-full justify-center pt-[83px]">
         <StakeContainer />
