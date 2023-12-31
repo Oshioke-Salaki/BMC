892206eb742d2d7f5f04aa08c1e411af722eaa95
Oshioke-Salaki <salaki1902@gmail.com>
2023-12-31 14:21:43 -0500

    end of year push

--- src/ui/complex/FaucetRequestModal.jsx
+++ src/ui/complex/FaucetRequestModal.jsx
@@ -7,7 +7,7 @@ function FaucetRequestModal() {
   return (
     <div className="bg-overlayPrimary absolute inset-0 z-[200] flex justify-center pt-[140px]">
       <div className=" shadow-shadowPrimary h-fit rounded-[5px] bg-white px-[45px] py-[27px]">
-        <RequestModalControl step={step} />
+        <RequestModalControl step={step} setStep={setStep} />
         <WaitingForConfirmationModal />
       </div>
     </div>
