6563b7f45baa770fbe9d1532327cf0c37c71b83f
collinsC10 <collins4chinonso@gmail.com>
2024-01-09 14:36:53 +0100

    Improve Animations

--- src/ui/components/CryptoInput.jsx
+++ src/ui/components/CryptoInput.jsx
@@ -3,7 +3,7 @@ import arrDown from "../../assets/down-arrow.svg";
 function CryptoInput({getSymbol, symbol, amount, setAmount}) {
   return (
     <div className="relative w-full">
-      <button className="absolute inset-y-0 left-0 cursor-pointer bg-transparent px-[7px] py-[9px]">
+      <button className="nice absolute inset-y-0 left-0 cursor-pointer bg-transparent px-[7px] py-[9px]">
         <div className="flex h-full items-center gap-x-1 rounded-[50px] border-[0.5px] border-solid border-[#c4c4c4] bg-[#430f5d] py-[9px] pl-[12px] pr-[30px] text-sm font-bold text-white">
           <img src={coinLogo} alt="" />
           <h4
